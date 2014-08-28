fc978f3ba12acc290aa0f0a78f1ee6443fd48a83
dlmather <david@nodeprime.com>
2014-08-28 16:24:58 -0700

    Fixed a big JARED bug and fixed handling of querystring vs. options

--- test.js
+++ test.js
@@ -1,20 +1,11 @@
 var Client = require('./lib');
 var client = new Client();
 console.log('Setting');
-client.setValue('hey/man/wam', 'hey', function(err) {
+client.setPath('this/is/great', function(err) {
   if (err) { console.log(err); }
-  console.log('Getting');
-  client.getValue('hey/man/wam', function(err, result) {
-    if (err) { console.log(err); }
-    client.updateTTL('k', '9', 9, function (err, res) {
-      if (err) {
-        console.error(err);
-      }
-      console.log('Deleting');
-      client.deletePath('hey', function (err) {
-        if (err) { console.log(err.message); }
-        console.log('we did it!');
-      });
-    })
+  console.log('Deleting');
+  client.deletePath('this/is/great', function (err) {
+    if (err) { console.log(err.message); }
+    console.log('we did it!');
   });
 });
