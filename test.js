5626185cde5c5e2c3c25acd6e5af46b9b45515a7
dlmather <david@nodeprime.com>
2014-08-27 17:11:53 -0700

    Improved (but still not perfect) error handling

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
 var Client = require('./lib');
 var client = new Client();
 console.log('Setting');
-client.setValue('wow/man', 'hey', function(err) {
+client.setValue('hey/man/wam', 'hey', function(err) {
   if (err) { console.log(err); }
   console.log('Getting');
-  client.getValue('wow/man', function(err, result) {
+  client.getValue('hey/man/wam', function(err, result) {
     if (err) { console.log(err); }
-    console.log(result);
-    console.log('Deleting');
-    client.deleteValue('wow/man', 'value', function (err) {
-      if (err) { console.log(err); }
-      console.log('we did it!');
-    });
+    client.updateTTL('k', '9', 9, function (err, res) {
+      if (err) {
+        console.error(err);
+      }
+      console.log('Deleting');
+      client.deletePath('hey', function (err) {
+        if (err) { console.log(err.message); }
+        console.log('we did it!');
+      });
+    })
   });
-})
+});
