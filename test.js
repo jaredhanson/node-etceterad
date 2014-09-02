7528f9efd839d4c767529967f96af09fbaa01390
dlmather <david@nodeprime.com>
2014-09-02 11:18:33 -0700

    Fixed where querystring fixes were used in the rest of the library.

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 var Client = require('./lib');
 var client = new Client();
 console.log('Setting');
-client.setPath('this/is/great', function(err) {
+
+client.setPath('/this/is/great', function (err) {
   if (err) { console.log(err); }
-  console.log('Deleting');
-  client.deletePath('this/is/great', function (err) {
-    if (err) { console.log(err.message); }
-    console.log('we did it!');
+  client.deletePath('/this/is/great', function (err) {
+    if (err) { console.log(err); }
   });
 });
